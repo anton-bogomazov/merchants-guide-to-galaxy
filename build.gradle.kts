2a853a191cbf34d212a992b1455f6263ef4c2050
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-10-16 15:06:38 +0200

    Configure test scope
    Add kotest runner and assertions

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 }
 
 tasks.test {
