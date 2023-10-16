2fa676b230ef69e0ce020097f3c7145e3bc3f4c2
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-10-16 17:35:31 +0200

    Extract buildSrc
    The module encapsulates knowledge about project dependencies and its versions.

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,8 @@
+object Libs {
+    object Versions {
+        const val kotest = "5.7.2"
+    }
+
+    const val kotest_runner = "io.kotest:kotest-runner-junit5:${Versions.kotest}"
+    const val kotest_assertions = "io.kotest:kotest-assertions-core:${Versions.kotest}"
+}
---
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -0,0 +1,8 @@
+object Plugins {
+    object Versions {
+        const val detekt = "1.23.1"
+    }
+
+    const val detekt = "io.gitlab.arturbosch.detekt"
+    const val detekt_formatting = "io.gitlab.arturbosch.detekt:detekt-formatting:${Versions.detekt}"
+}
