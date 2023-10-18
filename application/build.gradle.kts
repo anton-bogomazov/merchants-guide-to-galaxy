0e6e3c4fa04c4f75d085b33904f201ffcc81b95f
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-10-18 12:09:04 +0200

    Add ArrowKt
    I like to use Either for explicit functional error management

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -3,9 +3,12 @@ plugins {
 }
 
 dependencies {
+    implementation(Libs.arrow)
+
     testImplementation(Libs.kotest_runner)
     testImplementation(Libs.kotest_assertions)
     testImplementation(Libs.kotest_property)
+    testImplementation(Libs.kotest_arrow)
 
     testImplementation(Libs.mockk)
 }
--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,12 +1,19 @@
 object Libs {
     object Versions {
+        const val arrow = "1.2.0"
+
         const val kotest = "5.7.2"
+        const val kotest_arrow = "1.4.0"
         const val mockk = "1.13.8"
     }
+
+    const val arrow = "io.arrow-kt:arrow-core:${Versions.arrow}"
 
     const val kotest_runner = "io.kotest:kotest-runner-junit5:${Versions.kotest}"
     const val kotest_assertions = "io.kotest:kotest-assertions-core:${Versions.kotest}"
     const val kotest_property = "io.kotest:kotest-property:${Versions.kotest}"
+
+    const val kotest_arrow = "io.kotest.extensions:kotest-assertions-arrow:${Versions.kotest_arrow}"
 
     const val mockk = "io.mockk:mockk:${Versions.mockk}"
 }
