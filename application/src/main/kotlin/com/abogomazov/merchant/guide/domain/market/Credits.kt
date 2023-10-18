ee22bd7242026e2a372dedaeb7c0d7af86af3859
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-10-18 20:13:52 +0200

    Add market value objects
    Resource represents resource type: Dirt, Water.
    Credits represents currency and should be able to store big values (for totals).
    I chose BigInteger because there is no cents for credits. But I'm probably missing abstraction for resource/credit exchange rates. Add it later.

---
+++ application/src/main/kotlin/com/abogomazov/merchant/guide/domain/market/Credits.kt
@@ -0,0 +1,5 @@
+package com.abogomazov.merchant.guide.domain.market
+
+import java.math.BigInteger
+
+data class Credits(private val value: BigInteger)
---
+++ application/src/main/kotlin/com/abogomazov/merchant/guide/domain/market/Resource.kt
@@ -0,0 +1,3 @@
+package com.abogomazov.merchant.guide.domain.market
+
+data class Resource(private val value: String)
