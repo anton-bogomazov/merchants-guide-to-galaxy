67e38f78f6077dad1611cd36c006c2a1dcfa10c8
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-10-19 14:57:24 +0200

    Add GetResourceMarketPriceCommandParser

---
+++ application/src/main/kotlin/com/abogomazov/merchant/guide/parser/market/GetResourceMarketPriceCommandParser.kt
@@ -0,0 +1,59 @@
+package com.abogomazov.merchant.guide.parser.market
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.raise.either
+import arrow.core.right
+import com.abogomazov.merchant.guide.application.CommandParser
+import com.abogomazov.merchant.guide.cli.commands.Command
+import com.abogomazov.merchant.guide.cli.commands.GetResourceMarketPriceCommand
+import com.abogomazov.merchant.guide.parser.CommandArguments
+import com.abogomazov.merchant.guide.parser.CommandRegexBuilder
+import com.abogomazov.merchant.guide.parser.ParserError
+import com.abogomazov.merchant.guide.parser.toLocalNumber
+import com.abogomazov.merchant.guide.parser.toResource
+
+class GetResourceMarketPriceCommandParser(
+    private val command: String
+) : CommandParser {
+    companion object {
+        private val COMMAND_REGEX = CommandRegexBuilder()
+            .how().s().many().s().credits().s().iz().s().LocalNum().s().Resource().s().question()
+            .build()
+
+        fun match(command: String) = command.matches(COMMAND_REGEX)
+    }
+
+    override fun parse(): Either<ParserError, Command> =
+        extractArguments().map { (localNum, resource) ->
+            return GetMarketPriceArguments(
+                localNum.sanitized(),
+                resource
+            ).toCommand()
+        }
+
+    private fun extractArguments() = either<ParserError.FailedToExtractArguments, Pair<String, String>> {
+        val groups = COMMAND_REGEX.find(command)?.groups ?: return ParserError.FailedToExtractArguments.left()
+        val localNum = groups[1]?.value ?: return ParserError.FailedToExtractArguments.left()
+        val resource = groups[2]?.value ?: return ParserError.FailedToExtractArguments.left()
+
+        return Pair(localNum, resource).right()
+    }
+
+}
+
+fun String.sanitized() = this.replace("\\s+".toRegex(), " ").trim()
+
+private data class GetMarketPriceArguments(
+    private val localNum: String,
+    private val resource: String,
+): CommandArguments {
+    override fun toCommand() = either<ParserError.InvalidArguments, Command> {
+        return resource.toResource().map {
+            return GetResourceMarketPriceCommand(
+                localNum = localNum.toLocalNumber(),
+                resource = it,
+            ).right()
+        }
+    }
+}
