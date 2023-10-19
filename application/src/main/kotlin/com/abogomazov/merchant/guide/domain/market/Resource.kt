ad68efb63be873f6eaecfe3a3b4c9a31aa1a1de0
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-10-19 11:10:00 +0200

    Implement Resource Validations

--- application/src/main/kotlin/com/abogomazov/merchant/guide/domain/market/Resource.kt
+++ application/src/main/kotlin/com/abogomazov/merchant/guide/domain/market/Resource.kt
@@ -1,3 +1,28 @@
 package com.abogomazov.merchant.guide.domain.market
 
-data class Resource(private val value: String)
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import com.abogomazov.merchant.guide.domain.market.ResourceValidationError.*
+
+sealed interface ResourceValidationError {
+    data object EmptyString : ResourceValidationError
+    data object NonLetters : ResourceValidationError
+    data object MoreThanOneWord : ResourceValidationError
+}
+
+data class Resource internal constructor(private val value: String) {
+
+    companion object {
+        fun from(value: String) = either<ResourceValidationError, Resource> {
+            ensure(value.isNotBlank()) { EmptyString }
+            ensure(value.singleWord()) { MoreThanOneWord }
+            ensure(value.containsOnlyLetters()) { NonLetters }
+
+            Resource(value)
+        }
+    }
+
+}
+
+private fun String.singleWord() = this.split("\\s+".toRegex()).size == 1
+private fun String.containsOnlyLetters() = this.all { it.isLetter() }
---
+++ application/src/test/kotlin/com/abogomazov/merchant/guide/domain/market/ResourceTest.kt
@@ -0,0 +1,42 @@
+package com.abogomazov.merchant.guide.domain.market
+
+import io.kotest.assertions.arrow.core.shouldBeLeft
+import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.datatest.withData
+
+class ResourceTest : FreeSpec({
+
+    "any one worded string of letters is allowed" - {
+        withData(
+            nameFn = { "blank string: <$it>" },
+            "Water","dirt", "X", "BurBon"
+        ) {
+            Resource.from(it).shouldBeRight()
+        }
+    }
+
+    "impossible to create resource from blank string" - {
+        withData(
+            nameFn = { "blank string: <$it>" },
+            "", "  ", " \n "
+        ) {
+            Resource.from(it).shouldBeLeft(ResourceValidationError.EmptyString)
+        }
+    }
+
+    "impossible to create resource from non-letter string" - {
+        withData(
+            "123", "He11o", "$"
+        ) {
+            Resource.from(it).shouldBeLeft(ResourceValidationError.NonLetters)
+        }
+    }
+
+    "impossible to create resource from string with more than one word" - {
+        withData("Salt Water", "Gorges Blue Cheese", "R E S O U R C E") {
+            Resource.from(it).shouldBeLeft(ResourceValidationError.MoreThanOneWord)
+        }
+    }
+
+})
