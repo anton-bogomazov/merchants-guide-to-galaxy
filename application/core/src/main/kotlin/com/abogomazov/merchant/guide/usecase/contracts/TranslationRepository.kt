bd3b744b81d9c11dcea60f1d124a27971f8ba841
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-10-30 21:32:33 +0100

    Delete unused Market and Translation Repositories

--- application/core/src/main/kotlin/com/abogomazov/merchant/guide/usecase/contracts/TranslationRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.abogomazov.merchant.guide.usecase.contracts
-
-import com.abogomazov.merchant.guide.usecase.common.TranslationProvider
-import com.abogomazov.merchant.guide.usecase.translator.TranslationPersister
-import com.abogomazov.merchant.guide.usecase.translator.TranslationRemover
-
-interface TranslationRepository : TranslationProvider, TranslationPersister, TranslationRemover
